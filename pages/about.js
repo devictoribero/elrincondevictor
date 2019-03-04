8e04232f1da38abe78cb9262fdbc25bc0aeeefd7
Victor Ribero <victor.ribero3@gmail.com>
2019-03-04 17:06:07 +0100

    :tada: First commit

---
+++ pages/about.js
@@ -0,0 +1,28 @@
+function HelloWorld() {
+  return (
+    <div>
+      Hello world
+      <p>scoped!</p>
+      <style jsx>{`
+        p {
+          color: blue;
+        }
+        div {
+          background: yellow;
+        }
+        @media (max-width: 600px) {
+          div {
+            background: blue;
+          }
+        }
+      `}</style>
+      <style global jsx>{`
+        body {
+          background: red;
+        }
+      `}</style>
+    </div>
+  )
+}
+
+export default HelloWorld
