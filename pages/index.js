d30891005ea7c7815dc389d614079fde0d5f0eec
Victor Ribero <victor.ribero3@gmail.com>
2019-03-05 12:01:58 +0100

    commit to change computer

--- pages/index.js
+++ pages/index.js
@@ -1,28 +1,41 @@
-function HelloWorld() {
+import Head from 'next/head'
+
+export default function HelloWorld() {
   return (
-    <div>
-      Hello world
-      <p>scoped!</p>
+    <div className="page">
+      <Head>
+        <title>elrincóndevictor</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+
+      <div>
+        <h1>elrincóndevictor</h1>
+        <p className="slogan">Bienvenidos a mi mente</p>
+      </div>
+    
       <style jsx>{`
+        .page {
+          min-height: 100vh;
+          margin: 2rem;
+          background: red;
+          box-sizing: border-box;
+        }
+        
+        h1, p {
+          text-align: center;
+          margin: 0;
+        }
+        h1 {
+        }
         p {
-          color: blue;
-        }
-        div {
-          background: green;
-        }
-        @media (max-width: 600px) {
-          div {
-            background: blue;
-          }
         }
       `}</style>
       <style global jsx>{`
         body {
-          background: black;
+          margin: 0;
+          font-family: -apple-system,BlinkMacSystemFont,Segoe UI,Roboto,Oxygen,Ubuntu,Cantarell,Fira Sans,Droid Sans,Helvetica Neue,sans-serif;
         }
       `}</style>
     </div>
   )
 }
-
-export default HelloWorld
