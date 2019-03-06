00eed69630be5cc0acee16c2604b6ea7c09ffded
victor ribero <victor.ribero3@gmail.com>
2019-03-06 22:11:25 +0100

    twitter seo image changed

--- config/seo.js
+++ config/seo.js
@@ -9,13 +9,13 @@ export const DEFAULT_SEO = {
     title: 'El rincón de victor',
     description: 'Desarrollador web especializado en front-end y metodologías ágiles. Amante de la comunicación y la interacción humana.',
     image:
-      'https://prismic-io.s3.amazonaws.com/gary-blog%2F3297f290-a885-4cc6-9b19-3235e3026646_default.jpg',
+      'https://hdwallpaperfx.com/wp-content/uploads/2015/10/Apple-imac-Desk-Setup.jpg',
     site_name: 'Elrincondevictor.com',
     imageWidth: 1200,
     imageHeight: 1200
   },
   twitter: {
-    handle: '@garmeeh',
+    handle: '@JS_TUREEY',
     cardType: 'summary_large_image'
   }
 };
