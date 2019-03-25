6b1f46fa4cb0f911f77d7cca0802757910f2afb3
victor ribero <victor.ribero3@gmail.com>
2019-03-25 16:10:01 +0100

    Added publication details in Post

--- components/PostHeader.js
+++ components/PostHeader.js
@@ -1,4 +1,15 @@
+import {months} from '../helpers/months';
+
 export default function Post({post}) {
+  const publicationDate = new Date(post.date);
+  const day = publicationDate.getDate();
+  const month = months[publicationDate.getMonth()];
+  const year = publicationDate.getFullYear();
+  const author = {
+    name: 'Victor Ribero',
+    img_src: 'https://avatars0.githubusercontent.com/u/16169890?s=400&v=4'
+  }
+
   return (
     <header>
       <figure>
@@ -6,7 +17,17 @@ export default function Post({post}) {
       </figure>
       <h1>{post.title}</h1>
 
+      <PublicationDetails
+        day={day}
+        month={month}
+        year={year}
+        author={author}/>
+
       <style jsx global>{`
+        header {
+          margin-bottom: 3rem;
+        }
+
         figure {
           margin: 0;
           position: relative;
@@ -23,7 +44,7 @@ export default function Post({post}) {
           background: linear-gradient(rgba(255,255,255,0), rgba(255,255,255,1));
         }
 
-        img {
+        figure img {
           display: block;
           width: 100%;
           height: 200px;
@@ -38,11 +59,11 @@ export default function Post({post}) {
           font-size: 36px;
           line-height: 1.5;
           letter-spacing: 0.5px;
-          margin: -5rem 0 2rem 0;
+          margin: -5rem 0 0;
         }
 
         @media screen and (min-width: 768px) {
-          img {
+          figure img {
             height: 300px;
           }
 
@@ -50,10 +71,43 @@ export default function Post({post}) {
             text-shadow: 4px 3px var(--primary-400);
             font-size: 50px;
             line-height: 1.25;
-            margin: -7rem 0 2rem 0;
+            margin: -7rem 0 0 0;
           }
         }
       `}</style>
     </header>
+  )
+}
+
+function PublicationDetails({day, month, year, author}) {
+  return (
+    <span>
+      <img
+        src={author.img_src}
+        alt={`Fotografía de ${author.name}, el autor de este artículo.`}/>
+      {author.name},
+      <time>{day} {month} del {year}</time>.
+
+      <style jsx>{`
+        span {
+          display: flex;
+          align-items: center;
+          margin-top: 1rem;
+          color: var(--grey-600);
+        }
+
+        img {
+          width: 30px;
+          height: 30px;
+          margin-right: 0.25rem;
+          border-radius: 100%;
+          border: 2px solid var(--primary-400);
+        }
+
+        time {
+          margin-left: 0.25rem;
+        }
+      `}</style>
+    </span>
   )
 }
