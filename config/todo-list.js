ebac95a49ee2fc7d287738ebc4879e1171a3cd47
victor ribero <victor.ribero3@gmail.com>
2019-04-08 12:43:29 +0200

    added todoList file

---
+++ config/todo-list.js
@@ -0,0 +1,22 @@
+export const tasksTodoList = [
+  'Escribir un post del blog',
+  'Decidir que pijamas donar y ordenar el resto.',
+  'Decidir que ropa deportiva donar y ordenar el resto.',
+  'Decidir que jerseys donar y ordenar el resto.',
+  'Decidir que calzado donar y ordenar el resto.',
+  'Decidir que camisetas donar y ordenar el resto.',
+  'Decidir que camisas donar y ordenar el resto.',
+  'Decidir que chaquetas donar y ordenar el resto.',
+  'Decidir que abrigos donar y ordenar el resto.',
+  'Decidir de que calcetines y calzoncillos deshacerme.',
+  'Decidir de que libros quiero deshacerme y probar de venderlos.',
+  'Filtrar fotos del móvil, pasarlas a la nuve y eliminarlas de local.',
+]
+
+export const featuresTodoList = [
+  'Añadir compartir artículo en redes sociales',
+  'Añadir Post anterior y siguiente',
+  'Añadir apartado: ¿De dónde aprendo?',
+  'Añadir apartado: cosas que he aprendido',
+  'Progressive Web App (APP)',
+]
