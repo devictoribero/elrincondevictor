2ee6c223ed67923d3a16856b0105d074aa3dbbe7
Victor Ribero <victor.ribero3@gmail.com>
2020-05-14 17:08:50 +0200

    seo update

--- config/seo.js
+++ config/seo.js
@@ -1,6 +1,6 @@
 export const DEFAULT_SEO = {
   title: 'El rincón de victor | I share my philosophy of life and healthy lifestyle along with technical articles of software development.',
-  description: 'I\'m a software engineer specialized in front-end development and agile methodologies. I love the communication, share knowledge and all kinds of interactions.',
+  description: "I'm a software engineer who loves building projects and I'm specialized in front-end development and agile methodologies. I love the communication, share knowledge and all kinds of interactions.",
   lang: 'en',
   openGraph: {
     type: 'website',
