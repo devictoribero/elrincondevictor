ec0ffbfe533e0bbe49733cd89be21b445d716b51
victor ribero <victor.ribero@schibsted.com>
2020-05-15 19:43:24 +0200

    back to static

--- pages/_document.js
+++ pages/_document.js
@@ -9,7 +9,7 @@ export default class MyDocument extends Document {
           <meta name="viewport" content="width=device-width, initial-scale=1" />
           <link
 						rel="preload"
-						href="/public/css/index.css"
+						href="/static/css/index.css"
 						rel="stylesheet"
 					/>
           <link
