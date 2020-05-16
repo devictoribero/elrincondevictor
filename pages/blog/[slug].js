6be1897467a973959b4b51349afb0b458642cdf3
victor ribero <victor.ribero@schibsted.com>
2020-05-16 11:43:32 +0200

    fixed directory of social media

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -25,7 +25,7 @@ export default function Page({pathname, article}) {
       <SeoMetaInfo
         title={`${article.data.title} | Elrincondevictor`}
         description={`${article.data.description}`}
-        image={`https://elrincondevictor.com/public/img/social-media/${article.data.slug}.png`}
+        image={`https://elrincondevictor.com/static/img/social-media/${article.data.slug}.png`}
         canonical={`https://www.elrincondevictor.com/blog/${article.data.slug}`}/>
       <main>
         <article>
