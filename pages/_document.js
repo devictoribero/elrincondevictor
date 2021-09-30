8e473c71c5abde9ca7e5e04481924b5f03686351
victorcrystallize <victor@crystallize.com>
2021-09-29 23:13:46 -0600

    removed /public

--- pages/_document.js
+++ pages/_document.js
@@ -1,4 +1,4 @@
-import Document, { Head, Main, NextScript } from "next/document"
+import Document, { Head, Main, NextScript } from "next/document";
 
 export default class MyDocument extends Document {
   render() {
@@ -7,18 +7,17 @@ export default class MyDocument extends Document {
         <Head>
           <meta charSet="utf-8" />
           <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <link rel="preload" href="css/index.css" rel="stylesheet" />
           <link
-						rel="preload"
-						href="/static/css/index.css"
-						rel="stylesheet"
-					/>
-          <link href="https://fonts.googleapis.com/css2?family=Karla:wght@400;700&family=Ubuntu:wght@400;500;700&display=swap" rel="stylesheet"/> 
+            href="https://fonts.googleapis.com/css2?family=Karla:wght@400;700&family=Ubuntu:wght@400;500;700&display=swap"
+            rel="stylesheet"
+          />
         </Head>
         <body>
           <Main />
           <NextScript />
         </body>
       </html>
-    )
+    );
   }
 }
