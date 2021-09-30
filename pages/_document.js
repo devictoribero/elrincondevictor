e1d0b00f5dc345c594bc3b57889628e5c59f3253
victorcrystallize <victor@crystallize.com>
2021-09-29 23:24:56 -0600

    fix css import

--- pages/_document.js
+++ pages/_document.js
@@ -7,7 +7,7 @@ export default class MyDocument extends Document {
         <Head>
           <meta charSet="utf-8" />
           <meta name="viewport" content="width=device-width, initial-scale=1" />
-          <link rel="preload" href="css/index.css" rel="stylesheet" />
+          <link rel="preload" href="/css/index.css" rel="stylesheet" />
           <link
             href="https://fonts.googleapis.com/css2?family=Karla:wght@400;700&family=Ubuntu:wght@400;500;700&display=swap"
             rel="stylesheet"
